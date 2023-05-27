9050796dd50116503a2310b5cec354e6103c79df
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2023-05-26 22:42:51 -0300

    Configuração do Firebase Storage e variáveis de ambiente

--- app/src/libs/firebase.ts
+++ app/src/libs/firebase.ts
@@ -1,8 +1,14 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBdiIw0INlC9FLpDHi8VCYM0VYea-_XYI4",
-    authDomain: "galeriadefotos-e7a69.firebaseapp.com",
-    projectId: "galeriadefotos-e7a69",
-    storageBucket: "galeriadefotos-e7a69.appspot.com",
-    messagingSenderId: "770155266894",
-    appId: "1:770155266894:web:8547e366c921ff3a03d41f",
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
 };
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);

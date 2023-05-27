34b74bf8620545ee17f77820bb3e2f8b133294c6
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2023-05-26 22:29:23 -0300

    Instalação das bibliotecas e configurações iniciais do app

---
+++ app/src/libs/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBdiIw0INlC9FLpDHi8VCYM0VYea-_XYI4",
+    authDomain: "galeriadefotos-e7a69.firebaseapp.com",
+    projectId: "galeriadefotos-e7a69",
+    storageBucket: "galeriadefotos-e7a69.appspot.com",
+    messagingSenderId: "770155266894",
+    appId: "1:770155266894:web:8547e366c921ff3a03d41f",
+};
